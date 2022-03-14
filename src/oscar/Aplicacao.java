887309a88df0cc44ca34fdd9207247e0d888abd3
Nascimento <nascimentoeverton568@gmail.com>
2022-03-14 11:55:30 -0300

    Alterações em todas as classes

--- src/oscar/Aplicacao.java
+++ src/oscar/Aplicacao.java
@@ -21,55 +21,14 @@ public class Aplicacao {
         oscarfemale = new ManipulacaoArquivo("oscarfemale.csv");
 
     }
-    private void ganhadorMaisJovem(){
+
+    private void ganhadorMaisJovem() {
         System.out.println("Ganhador mais Jovem: ");
         List<Oscar> roster = ManipulacaoArquivo.getRoster();
-        roster.stream().sorted(Comparator.comparing(Oscar.getAge)).findFirst();
+        roster.stream().sorted(Comparator.comparing(Oscar::getAge)).findFirst();
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private void atrizMaisJovemGanhouOscar() {
-    }
-
-    private void atorMaisJovemGanhouOscar() {
-    }
-
-
-    public void abrindoArquivoParaLeitura(){
-            try (Stream<String> stream = Files.lines(Paths.get(System.getProperty("Users.nasci"), "oscarfemale.csv"))) {
-                List<String> linhas = stream.collect(Collectors.toList());
-                List<Oscar> oscars = converteEmObjeto(linhas);
-                oscars.forEach(System.out::println);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    private static List<Oscar> converteEmObjeto(List<String> linhas) {
-        List<Oscar> resultado = new ArrayList<>();
-        for (String linha : linhas) {
-            String[] campos = linha.split(";");
-            Oscar novoOscar = new Oscar(
-                    Integer.parseInt(campos[2]),
-                    Integer.parseInt(campos[1]),
-                    Integer.parseInt(campos[3]),
-                    campos[4],
-                    campos[5]
-            );
-            resultado.add(novoOscar);
-        }
-        return resultado;
+    private void ganhadoraMaisJovem() {
+        System.out.println("Ganhadora mais jovem: ");
     }
 }
